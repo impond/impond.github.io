3ee1181033995bfe1bc9a5abbe1b8d7d6b4921a8
Joaquin <joaquin.perrone@gmail.com>
2025-07-01 14:14:48 -0400

    Update homepage text and fixed icons

--- koken/storage/configuration/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-	return array(
-		'hostname' => 'rdbms.strato.de',
-		'database' => 'DB2702457',
-		'username' => 'U2702457',
-		'password' => 'Blubbels-1',
-		'prefix' => 'koken2_',
-		'socket' => ''
-	);
